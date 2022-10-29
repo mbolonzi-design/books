0305c75ca73aebbfc0aec58945e12054707a5489
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-29 08:18:10 +0300

    create review  controller routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+    def index 
+        @reviews = Review.all
+        render json: @reviews
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:comment, :rating)
+    end
 end
