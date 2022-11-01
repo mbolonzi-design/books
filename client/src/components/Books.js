81a1760504441a79e6293a81c4d2ce8e04e8ba16
mbolonzi-design <mbolonzi.design@gmail.com>
2022-11-01 09:49:20 +0300

    create client components template

---
+++ client/src/components/Books.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+const Books = () => {
+    return (
+        <div>
+        <h1>Books</h1>
+        </div>
+    );
+};
+
+export default Books;
---
+++ client/src/components/NewBookForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewBookForm = () => {
+    return (
+        <div>
+        <h1>New Book Form</h1>
+        </div>
+    );
+}
+
+export default NewBookForm;
